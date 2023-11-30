ae9c8ebb76ea3030572cea4f98da1242b8b44c7f
Sobin <sobingeneral@gmail.com>
2023-11-30 15:56:25 +0700

    init commit

---
+++ src/main/java/dev/tveir/backendmessage/BackendmessageApplication.java
@@ -0,0 +1,13 @@
+package dev.tveir.backendmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendmessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendmessageApplication.class, args);
+	}
+
+}
