c870e1a1dde5a20708979462a762147d84d99289
Sobin <sobingeneral@gmail.com>
2023-11-30 16:40:24 +0700

    feat: add controller

--- src/main/java/dev/tveir/backendmessage/controllers/MessagesController.java
+++ src/main/java/dev/tveir/backendmessage/controllers/MessagesController.java
@@ -1,2 +1,16 @@
-package dev.tveir.backendmessage.controllers;public class MessagesController {
+package dev.tveir.backendmessage.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MessagesController {
+    @GetMapping("/messages")
+    public ResponseEntity<List<String>> message() {
+        return ResponseEntity.ok(Arrays.asList("first", "second"));
+    }
 }
