de97ba70a3da98e597246323f452e9d0a24fa83e
Sobin <sobingeneral@gmail.com>
2023-12-01 17:56:38 +0700

    fix: fix be able to register with same username

--- src/main/java/dev/tveir/backendmessage/auth/AuthenticationService.java
+++ src/main/java/dev/tveir/backendmessage/auth/AuthenticationService.java
@@ -5,10 +5,12 @@ import dev.tveir.backendmessage.user.Role;
 import dev.tveir.backendmessage.user.User;
 import dev.tveir.backendmessage.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,10 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
+        // check if username is already registered
+        if (repository.existsByUsername(request.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists");
+        }
         var user = User.builder()
                 .username(request.getUsername())
                 .password(passwordEncoder.encode(request.getPassword()))
