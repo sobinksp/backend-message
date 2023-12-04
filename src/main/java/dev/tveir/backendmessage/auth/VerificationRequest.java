993e0acd524cb06ec49057b06e243f76d239a00b
Sobin <sobingeneral@gmail.com>
2023-12-04 15:44:57 +0700

    fix: import lombok

--- src/main/java/dev/tveir/backendmessage/auth/VerificationRequest.java
+++ src/main/java/dev/tveir/backendmessage/auth/VerificationRequest.java
@@ -3,7 +3,7 @@ package dev.tveir.backendmessage.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
