5f174dfc92667252cf3a334510225910654582fc
Sobin <sobingeneral@gmail.com>
2023-12-18 21:26:24 +0700

    feat: websocket chatroom

--- src/main/java/dev/tveir/backendmessage/controller/ChatRoomController.java
+++ src/main/java/dev/tveir/backendmessage/controller/ChatRoomController.java
@@ -4,6 +4,10 @@ import dev.tveir.backendmessage.message.ChatRoom;
 import dev.tveir.backendmessage.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.List;
 public class ChatRoomController {
 
     private final ChatRoomService service;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @PostMapping
     public ResponseEntity<ChatRoom> createChatRoom(
@@ -24,12 +29,26 @@ public class ChatRoomController {
         return ResponseEntity.ok(service.createChatRoom(request));
     }
 
-
     @GetMapping("/{id}")
     public ResponseEntity<List<ChatRoom>>  getMemberChatRoomId(
             @PathVariable Integer id
     ) {
         return ResponseEntity.ok(service.getUserChatRoomId(id));
+    }
+
+    @MessageMapping("/create-chat-room")
+    public void createAndNotifyChatRoom(
+            @Payload ChatRoom chatroom
+    ) {
+        ChatRoom createdChatRoom = service.createChatRoom(chatroom);
+        messagingTemplate.convertAndSendToUser(
+                String.valueOf(chatroom.getMembers().get(1)),
+                "/queue/chatroom",
+                ChatRoom.builder()
+                        .id(createdChatRoom.getId())
+                        .members(createdChatRoom.getMembers())
+                        .build()
+                );
     }
 }
 
